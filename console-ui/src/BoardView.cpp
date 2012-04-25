a3d6f270b2053d100104d3a3ef66534948c2f371
Victor Portnov <vic.portnov@gmail.com>
2012-04-25 19:42:27 +0400

    [CONSILE UI] BoardView (see #3) is implemented in draft
    This is simple implementation of console painter for
    Board.
    It draws:
    '#' for white square
    ' ' (space) for empty black square
    'w' for white draught
    'W' for white king
    'b' for black draught
    'B' for black king
    
    Next step is to decorate it by frame and letters.

--- console-ui/src/BoardView.cpp
+++ console-ui/src/BoardView.cpp
@@ -1,1 +1,127 @@
+#include "BoardView.h"
 #include "Board.h"
+#include "RulesOfGame.h"
+#include "Coord.h"
+#include "Color.h"
+
+using namespace Checkers::Engine;
+using namespace Checkers::ConsoleUi;
+
+
+const char whiteSquareChar = '#';
+const char blackSquareChar = ' ';
+
+const char whiteDraught = 'w';
+const char whiteKingDraught = 'W';
+
+const char blackDraught = 'b';
+const char blackKingDraught = 'B';
+
+BoardView::BoardView()
+  : m_Board(0)
+  , m_Rotation(ERotation0)
+  , m_CurrentLine(0)
+{
+}
+
+BoardView::BoardView(const Engine::Board &aBoard, Rotation aRotation)
+  : m_Board(&aBoard)
+  , m_Rotation(aRotation)
+  , m_CurrentLine(0)
+{
+}
+
+void BoardView::setBoard(const Engine::Board &aBoard)
+{
+  m_Board = &aBoard;
+}
+
+BoardView::Rotation BoardView::rotation() const
+{
+  return m_Rotation;
+}
+
+void BoardView::setRotation(Rotation aRotation)
+{
+  m_Rotation = aRotation;
+}
+
+std::string BoardView::firstLine()
+{
+  m_CurrentLine = 0;
+  return flushLine(m_CurrentLine);
+}
+
+std::string BoardView::nextLine()
+{
+  return flushLine(++m_CurrentLine);
+}
+
+std::string BoardView::flushWhole()
+{
+  std::string ret;
+
+  for(std::string it = firstLine(); !it.empty(); it = nextLine())
+  {
+    ret += it += '\n';
+  }
+
+  return ret;
+}
+
+std::string BoardView::flushLine(int aN) const
+{
+  std::string ret;
+
+  if(Engine::RulesOfGame::BoardBounds::isLineInBound(aN))
+  {
+    int height = RulesOfGame::BoardBounds::height();
+    int width = RulesOfGame::BoardBounds::width();
+
+    int i = m_Rotation == ERotation0 ? height - 1 - aN : aN;
+
+    for(int m = 0; m < width; ++m)
+    {
+      int j = m_Rotation == ERotation0 ? m : width - 1 - m;
+
+      if((i + j) % 2)
+      {
+        ret += whiteSquareChar;
+      }
+      else if(m_Board)
+      {
+        Maybe<Draught> ijSquare = m_Board->testSquare(Coord(i,j));
+
+        if(ijSquare.isNothing())
+        {
+          ret += blackSquareChar;
+        }
+        else if(ijSquare().color() == Color::EWhite)
+        {
+          ret += ijSquare().isKing() ? whiteKingDraught : whiteDraught;
+        }
+        else
+        {
+          ret += ijSquare().isKing() ? blackKingDraught : blackDraught;
+        }
+      }
+      else
+      {
+        ret += blackSquareChar;
+      }
+    }
+  }
+
+  return ret;
+}
+
+std::ostream & Checkers::ConsoleUi::operator<<(std::ostream &aOut, BoardView &aBoardView)
+{
+  return aOut << aBoardView.flushWhole();
+}
+
+std::ostream & Checkers::ConsoleUi::operator<<(std::ostream &aOut, const Board &aBoard)
+{
+  BoardView view(aBoard);
+  return aOut << view;
+}
---
+++ console-ui/src/BoardView.h
@@ -0,0 +1,53 @@
+#ifndef H_BOARD_VIEW_H
+#define H_BOARD_VIEW_H
+
+#include <string>
+
+
+namespace Checkers
+{
+  namespace Engine
+  {
+    class Board;
+  }
+
+  namespace ConsoleUi
+  {
+    class BoardView
+    {
+    public:
+      enum Rotation
+      {
+        ERotation0
+      , ERoatation180
+      };
+
+    public:
+      BoardView();
+      BoardView(const Engine::Board &aBoard, Rotation aRotation = ERotation0);
+
+      void setBoard(const Engine::Board &aBoard);
+
+      Rotation rotation() const;
+      void setRotation(Rotation aRotation);
+
+      std::string firstLine();
+      std::string nextLine();
+
+      std::string flushWhole();
+
+    private:
+      std::string flushLine(int aN) const;
+
+    private:
+      const Engine::Board *m_Board;
+      Rotation m_Rotation;
+      int m_CurrentLine;
+    };
+
+    std::ostream & operator<<(std::ostream &aOut, BoardView &aBoardView);
+    std::ostream & operator<<(std::ostream &aOut, const Engine::Board &aBoard);
+  };
+};
+
+#endif //H_BOARD_VIEW_H
